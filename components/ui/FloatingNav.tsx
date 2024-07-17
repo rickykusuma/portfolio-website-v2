0846d296b16dd1b678f913a39fe56787917d8c20
Ricky Andrianto Kusuma <ricky@codr.id>
2024-07-17 22:15:20 +0700

    Fix(Floating Nav): Fix initial y floating nav

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -46,14 +46,14 @@ export const FloatingNav = ({
       <motion.div
         initial={{
           opacity: 1,
-          y: -100,
+          y: -200,
         }}
         animate={{
-          y: visible ? 0 : -100,
+          y: visible ? 0 : -200,
           opacity: visible ? 1 : 0,
         }}
         transition={{
-          duration: 0.2,
+          duration: 0.3,
         }}
         className={cn(
           "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border bg-black-100 dark:border-white/[0.2] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white-/[0.2]",
