0586d6766a6a705ad61cda84aa24443a389a4708
Ricky Andrianto Kusuma <ricky@codr.id>
2024-07-17 22:12:29 +0700

    Feat(Logo & Images): Added Logo for icon web & background image for grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,27 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ricky's Portfolio",
+  title: "Ricky | Front-end Developer",
   description: "Freelancer, Front-end developer, Portfolio, Personal Website",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({
